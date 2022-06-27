bb65ad029df25407ef6486b6daf608e13de2c814
mashadinoor <mashadinoor@gmail.com>
2022-06-27 12:28:21 +0700

    add "header section"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderShaynaVue />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShaynaVue from '@/components/HeaderShayna.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderShaynaVue
   }
 }
 </script>
